9b5e9a428f46f2abf45efeee4ff359f11e20af96
KaiqueSP96 <kaiqueba6@gmail.com>
2024-02-27 21:56:11 -0300

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//States:
+//Initial States:
 let xp = 0;
 let health = 100;
 let gold = 50;
@@ -7,12 +7,22 @@ let fighting;
 let monsterHealth;
 let inventory = ["stick"];
 
+//Arrays
+const locations = [
+    {
+        name: "town square",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"],
+        "button functions": [goStore, goCave, fightDragon],
+        text: "You are in the town square. You see a sign that says \"Store\"."
+    }
+];
+
 //Buttons Selectors:
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 
-// Text Selectors:
+//HTML Selectors:
 const text = document.querySelector("#text");
 const xpText = document.querySelector("#xpText")
 const healthText = document.querySelector("#healthText");
@@ -27,10 +37,24 @@ button2.onclick = goCave;
 button3.onclick = fightDragon; 
 
 //Functions:
+function goTown() {
+    button1.innerText = "Go to store";
+    button2.innerText = "Go to cave";
+    button3.innerText = "Fight dragon";
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+}
+
 function goStore() {
     button1.innerText = "Buy 10 health (10 gold)";
     button2.innerText = "Buy weapon (30 gold)";
     button3.innerText = "Go to town square";
+    text.innerText = "You enter the store.";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
 }
 
 function goCave() {
