68aa092f083655a6650b369a8feec276fdab2ae9
KaiqueSP96 <kaiqueba6@gmail.com>
2024-03-07 10:43:27 -0300

    add function sellWeapon

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ function goCave() {
 
 
 function buyWeapon() {
-  if (currentWeapon < weapons.length) {
+  if (currentWeapon < weapons.length - 1) {
     if (gold >= 30) {
       gold -= 30;
       currentWeapon++;
@@ -105,10 +105,23 @@ function buyWeapon() {
     } else {
       text.innerText = "You do not have enough gold to buy a weapon."
     }
+  } else {
+    text.innerText = "You already have the most powerful weapon!"
+    button2.innerText = "Sell weapon for 15 gold"
+    button2.onclick = sellWeapon();
   }
   //weapons[currentWeapon] = weapons[0] = {...stick}
   //weapons[currentWeapon] = weapons[1] = {...dagger}
 }
+
+function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+  }
+};
 
 function fightDragon() {
   console.log("Fighting dragon.");
