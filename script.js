f0d8f8949816bbf4334af4504f215c0ff23a95c7
KaiqueSP96 <kaiqueba6@gmail.com>
2024-03-13 12:30:28 -0300

    add one more location / add function restart / add function lose

--- script.js
+++ script.js
@@ -85,6 +85,18 @@ const locations = [
     "button text": ["Attack", "Dodge", "Run"],
     "button functions": [attack, dodge, goTown],
     text: "You are fighting a monster."
+  },
+  {
+    name: "kill monster",
+    "button text": ["Go to town square", "Go to town square", "Go to town square"],
+    "button functions": [goTown, goTown, goTown],
+    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+  },
+  {
+    name: "lose",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You die. &#x2620;"
   }
 ];
 
@@ -96,6 +108,7 @@ button3.onclick = fightDragon;
 
 //Functions:
 function update(location) {
+  monsterStats.styles.display = 'none';
   button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
   button3.innerText = location["button text"][2];
@@ -188,9 +201,24 @@ function defeatMonster() {
   xp += monsters[fighting].level
   goldText.innerText = gold
   xpText.innerText = xp
+  update(locations[4])
 }
 
-function lose() { }
+function lose() {
+  update(locations[5])
+}
+
+function restart() {
+  xp = 0;
+  health = 100;
+  gold = 50;
+  currentWeapon = 0;
+  inventory = ["stick"]
+  goldText.innerText = gold;
+  healthText.innerText = health;
+  xpText.innerText = xp;
+  goTown();
+}
 
 function fightSlime() {
   fighting = 0
